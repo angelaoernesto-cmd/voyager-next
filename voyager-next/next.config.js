18ca5ba9a51b051898ef4d1e84fd28521b31c3da
angelaoernesto-cmd <angelaoernesto@gmail.com>
2026-05-17 15:06:48 +0200

    Update next.config.js

--- voyager-next/next.config.js
+++ voyager-next/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Si se ejecuta en internet (Vercel), desactiva el modo export para que funcione la API.
-  // Si compila en tu ordenador, activa 'export' para meter los archivos en el móvil.
+  // Si compila en Vercel, desactiva 'export' para que cree la API.
+  // Si compila en tu ordenador, activa 'export' para generar la carpeta 'out' del móvil.
   output: process.env.VERCEL ? undefined : 'export',
   reactStrictMode: false,
   images: {
